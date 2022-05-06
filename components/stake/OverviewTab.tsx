ea9e286b28af6bfe46049dec956e30601d63050a
CodeWarrier <CodeWarrier3000@gmail.com>
2022-05-07 00:30:06 +0900

    updated

--- components/stake/OverviewTab.tsx
+++ components/stake/OverviewTab.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import * as React from 'react';
 import CustomInput from './CustomInput';
 import SellIcon from '@mui/icons-material/Sell';
@@ -18,138 +18,146 @@ const OverviewTab = () => {
       >
         Earn up to 3% daily with Stake Protocol's Net Elastic Rebase Depletion
       </Box>
-      <Box
+      <Grid
+        container
+        spacing={5}
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          gap: '80px',
-          paddingTop: '20px'
+          padding: '30px'
         }}
       >
-        <Box
-          sx={{
-            width: '40%',
-            background: '#212121',
-            borderRadius: '16px',
-            padding: '20px'
-          }}
+        <Grid
+          item
+          xs={6}
         >
           <Box
             sx={{
-              fontSize: '20px',
-              fontWeight: 'bold',
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            Total Stake Available
-          </Box>
-          <Box
-            sx={{
-              fontSize: '26px',
-              fontWeight: 'bold',
-              display: 'flex',
-              justifyContent: 'center'
-            }}
-          >
-            123.3211
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            $3,532.23432
-          </Box>
-          {/* buttons */}
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              gap: '20px',
-              paddingTop: '20px'
+              background: '#212121',
+              padding: '20px',
+              borderRadius: '16px'
             }}
           >
             <Box
               sx={{
-                width: '50%'
+                fontSize: '20px',
+                fontWeight: 'bold',
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: '20px'
               }}
             >
-              <Button fullWidth variant="contained">Claim all</Button>
+              Total Stake Available
             </Box>
             <Box
               sx={{
-                width: '50%'
+                fontSize: '26px',
+                fontWeight: 'bold',
+                display: 'flex',
+                justifyContent: 'center'
               }}
             >
-              <Button fullWidth variant="contained">Compound all</Button>
+              123.3211
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              $3,532.23432
+            </Box>
+            {/* buttons */}
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                gap: '20px',
+                paddingTop: '20px'
+              }}
+            >
+              <Box
+                sx={{
+                  width: '50%'
+                }}
+              >
+                <Button size="large" fullWidth variant="contained">Claim all</Button>
+              </Box>
+              <Box
+                sx={{
+                  width: '50%'
+                }}
+              >
+                <Button size="large" fullWidth variant="contained">Compound all</Button>
+              </Box>
+            </Box>
+            <Box sx={{ marginTop: '20px' }}>
+              <Button size="large" fullWidth variant="contained">Deposit</Button>
             </Box>
           </Box>
-          <Box sx={{marginTop: '20px'}}>
-            <Button fullWidth variant="contained">Deposit</Button>
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            width: '40%',
-            background: '#212121',
-            borderRadius: '16px',
-            padding: '40px'
-          }}
+        </Grid>
+        <Grid
+          item
+          xs={6}
         >
-          {/* sell */}
           <Box
             sx={{
-              display: 'flex',
+              padding: '20px',
+              background: '#212121',
+              borderRadius: '16px'
             }}
           >
-            <Box>
-              Sell Stake
+            {/* sell */}
+            <Box
+              sx={{
+                display: 'flex',
+              }}
+            >
+              <Box>
+                Sell Stake
+              </Box>
+              <Box sx={{ flexGrow: 1 }}></Box>
+              <Box>Stake Balance: 65,707</Box>
             </Box>
-            <Box sx={{flexGrow: 1}}></Box>
-            <Box>Stake Balance: 65,707</Box>
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginBottom: '10px'
-            }}
-          >
-            <CustomInput icon={<SellIcon/>}/>
-          </Box>
-          <Box sx={{display: 'flex', justifyContent: 'right'}}>
-            <Button variant="contained">Sell</Button>
-          </Box>
-          {/* buy */}
-          <Box
-            sx={{
-              display: 'flex',
-              paddingLeft: '40px',
-              paddingRight: '40px'
-            }}
-          >
-            <Box>
-              Buy Stake
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: '10px'
+              }}
+            >
+              <CustomInput icon={<SellIcon />} />
             </Box>
-            <Box sx={{flexGrow: 1}}></Box>
-            <Box>BNB Balance: 1.452</Box>
+            <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+              <Button variant="contained">Sell</Button>
+            </Box>
+            {/* buy */}
+            <Box
+              sx={{
+                display: 'flex',
+                paddingLeft: '40px',
+                paddingRight: '40px'
+              }}
+            >
+              <Box>
+                Buy Stake
+              </Box>
+              <Box sx={{ flexGrow: 1 }}></Box>
+              <Box>BNB Balance: 1.452</Box>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: '10px'
+              }}
+            >
+              <CustomInput icon={<ShoppingBagIcon />} />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+              <Button variant="contained">Buy</Button>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginBottom: '10px'
-            }}
-          >
-            <CustomInput icon={<ShoppingBagIcon/>}/>
-          </Box>
-          <Box sx={{display: 'flex', justifyContent: 'right'}}>
-            <Button variant="contained">Buy</Button>
-          </Box>
-        </Box>
-      </Box>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
