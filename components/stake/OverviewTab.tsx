f7f2e433f93c20e446749905a12fb79e81862f7a
CodeWarrier <CodeWarrier3000@gmail.com>
2022-05-07 03:02:13 +0900

    updated

--- components/stake/OverviewTab.tsx
+++ components/stake/OverviewTab.tsx
@@ -20,7 +20,7 @@ const OverviewTab = () => {
       </Box>
       <Grid
         container
-        spacing={5}
+        spacing={2}
         sx={{
           padding: '30px'
         }}
@@ -155,6 +155,383 @@ const OverviewTab = () => {
             <Box sx={{ display: 'flex', justifyContent: 'right' }}>
               <Button variant="contained">Buy</Button>
             </Box>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+        >
+          <Box
+            sx={{
+              background: '#212121',
+              padding: '20px',
+              borderRadius: '16px'
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '18px'
+                  }}
+                >
+                  NFV
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    margin: '10px'
+                  }}
+                >
+                  (Net Faucet Value)
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
+                  1,865,707
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                  $65,707
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '18px'
+                  }}
+                >
+                  GFV
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    margin: '10px'
+                  }}
+                >
+                  (Gross Faucet Value)
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
+                  1,865,707
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                  $65,707
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '18px'
+                  }}
+                >
+                  Referral Rewards
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
+                  1,865,707
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                  $65,707
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '18px'
+                  }}
+                >
+                  FLAME in Wallet
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
+                  707
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                  $5,707
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+        >
+          <Box
+            sx={{
+              background: '#212121',
+              padding: '20px',
+              borderRadius: '16px'
+            }}
+          >
+            {/* NERD */}
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                fontWeight: 'bold',
+                fontSize: '20px'
+              }}
+            >
+              NERD
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              (Net Elastic Rebase Depletion)
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+        >
+          <Box
+            sx={{
+              background: '#212121',
+              padding: '20px',
+              borderRadius: '16px'
+            }}
+          >
+            {/* market cap */}
+            <Grid container spacing={1}>
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Market Cap
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  $999,999,999
+                </Box>
+              </Grid>
+              {/* Total players */}
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Total Players
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  99,999
+                </Box>
+              </Grid>
+              {/* faucet returns */}
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Faucet Returns
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  1% Daily
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  (on locked value)
+                </Box>
+              </Grid>
+              {/* Treasury Value */}
+              <Grid item xs={3}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Treasury Value
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  $999,999,999
+                </Box>
+              </Grid>
+              {/* Total POL */}
+              <Grid item xs={3}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Total POL
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  $999,999,999
+                </Box>
+              </Grid>
+              {/* total supply */}
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Total Supply
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  1,865,707
+                </Box>
+              </Grid>
+              {/* Locked in Vault */}
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Locked in Vault
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  65,707
+                </Box>
+              </Grid>
+              {/* rebase returns */}
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Rebase Returns
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  2% Daily
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  (on locked value)
+                </Box>
+              </Grid>
+              {/* floor price */}
+              <Grid item xs={3}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Floor Price
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  65,707
+                </Box>
+              </Grid>
+              {/* time until next rebase */}
+              <Grid item xs={3}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Time until next rebase
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  23 min
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+        >
+          <Box
+            sx={{
+              background: '#212121',
+              padding: '20px',
+              borderRadius: '16px'
+            }}
+          >
+            card
           </Box>
         </Grid>
       </Grid>
